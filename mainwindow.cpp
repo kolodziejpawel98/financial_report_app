10de64a9e5dbc4845fafe1191fbf90c1c0b89052
Pawel Kolodziej <kolodziejpawel98@gmail.com>
2025-07-20 23:39:44 +0200

    expenses lists

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,56 +2,91 @@
 #include "./ui_mainwindow.h"
 
 QT_CHARTS_USE_NAMESPACE
-
+std::ostream &operator<<(std::ostream &os, const QPoint &point)
+{
+    os << "(" << point.x() << ", " << point.y() << ")";
+    return os;
+}
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    allExpensesLabelsContainers = {ui->groceriesLabelsContainer,
+                                   ui->nonGroceriesLabelsContainer,
+                                   ui->citySpendingsLabelsContainer,
+                                   ui->transportationLabelsContainer,
+                                   ui->regularPaymentsLabelsContainer,
+                                   ui->photographyLabelsContainer,
+                                   ui->otherSpendingsLabelsContainer};
+
+    allExpensesLabelsHeadings = {ui->groceriesHeading,
+                                 ui->nonGroceriesHeading,
+                                 ui->citySpendingsHeading,
+                                 ui->transportationHeading,
+                                 ui->regularPaymentsHeading,
+                                 ui->photographyHeading,
+                                 ui->otherSpendingsHeading};
     connect(ui->loadXmlButton, &QPushButton::clicked, this, &MainWindow::loadXmlButtonClicked);
+    // for (auto &expenseContainer : allExpensesLabelsContainers)
+    // {
+    //     drawExpensesLabels(expenseContainer);
+    // }
+    std::vector<std::vector<std::string>> expenseContainers = {
+        {"12.99 PLN", "25.50 PLN", "33.00 PLN"},
+        {"199.99 PLN", "20.00 PLN"},
+        {"5.50 PLN", "9.99 PLN", "49.90 PLN", "15.00 PLN"},
+        {"100.00 PLN", "200.00 PLN", "300.00 PLN", "150.50 PLN", "50.25 PLN", "100.00 PLN", "200.00 PLN", "300.00 PLN", "150.50 PLN", "50.25 PLN", "100.00 PLN", "200.00 PLN", "300.00 PLN", "150.50 PLN", "50.25 PLN", "100.00 PLN", "200.00 PLN", "300.00 PLN", "150.50 PLN", "50.25 PLN", "100.00 PLN", "200.00 PLN", "300.00 PLN", "150.50 PLN", "50.25 PLN", "100.00 PLN", "200.00 PLN", "300.00 PLN", "150.50 PLN", "50.25 PLN", "100.00 PLN", "200.00 PLN", "300.00 PLN", "150.50 PLN", "50.25 PLN", "100.00 PLN", "200.00 PLN", "300.00 PLN", "150.50 PLN", "50.25 PLN", "100.00 PLN", "200.00 PLN", "300.00 PLN", "150.50 PLN", "50.25 PLN", "100.00 PLN", "200.00 PLN", "300.00 PLN", "150.50 PLN", "50.25 PLN", "100.00 PLN", "200.00 PLN", "300.00 PLN", "150.50 PLN", "50.25 PLN"},
+        {"12.00 PLN"},
+        {"8.00 PLN", "11.99 PLN", "14.50 PLN", "18.20 PLN"},
+        {"299.99 PLN", "199.00 PLN", "150.00 PLN"}};
 
-    int x = 0;
-    int y = 0;
-    int spacing = 5;
-    int maxWidth = ui->groceriesLabelsContainer->width();
-    int numOfExpenses = 65;
+    QPoint p0{290, 110};
+    QPoint p1 = drawExpensesLabels(ui->groceriesLabelsContainer, ui->groceriesHeading, 16, expenseContainers[0], p0);
+    QPoint p2 = drawExpensesLabels(ui->nonGroceriesLabelsContainer, ui->nonGroceriesHeading, 32, expenseContainers[1], p1);
+    QPoint p3 = drawExpensesLabels(ui->citySpendingsLabelsContainer, ui->citySpendingsHeading, 10, expenseContainers[2], p2);
+    QPoint p4 = drawExpensesLabels(ui->transportationLabelsContainer, ui->transportationHeading, 4, expenseContainers[3], p3);
+    QPoint p5 = drawExpensesLabels(ui->regularPaymentsLabelsContainer, ui->regularPaymentsHeading, 12, expenseContainers[4], p4);
+    QPoint p6 = drawExpensesLabels(ui->photographyLabelsContainer, ui->photographyHeading, 9, expenseContainers[5], p5);
+    QPoint p7 = drawExpensesLabels(ui->otherSpendingsLabelsContainer, ui->otherSpendingsHeading, 33, expenseContainers[6], p6);
+}
 
-    for (int i = 0; i < numOfExpenses; ++i)
+QPoint MainWindow::drawExpensesLabels(QWidget *labelsContainer, QLabel *labelsContainerHeader, int numOfExpenses, const std::vector<std::string> &prices, QPoint movingPoints)
+{
+    labelsContainerHeader->move(movingPoints.x(), movingPoints.y() + 20);
+    labelsContainer->move(movingPoints.x(), movingPoints.y() + 40);
+    std::cout << "x: " << movingPoints.x() << " y: " << movingPoints.y() << std::endl;
+    int xCoordinateOfLabelInContainer = 0;
+    int yCoordinateOfLabelInContainer = 0;
+    int spacingBetweenLabels = 5;
+    int maxWidthOfLabelsContainer = labelsContainer->width();
+
+    for (int i = 0; i < prices.size(); ++i)
     {
-        QLabel *label = new QLabel(QString("%1 PLN").arg((i + 1) * 10.32), ui->groceriesLabelsContainer);
+        QLabel *label = new QLabel(QString::fromStdString(prices[i]), labelsContainer);
         label->setStyleSheet("color: black; font-size: 12px;");
         label->adjustSize();
         label->setCursor(Qt::PointingHandCursor);
         label->installEventFilter(this);
-        priceLabels.push_back(label);
+        expenseLabels.push_back(label);
 
-        int predictedWidth = x + label->width();
+        int predictedWidthOfLabel = xCoordinateOfLabelInContainer + label->width();
         if (i < numOfExpenses - 1)
-            predictedWidth += spacing + 10;
+            predictedWidthOfLabel += spacingBetweenLabels + 10;
 
-        if (predictedWidth > maxWidth)
+        if (predictedWidthOfLabel > maxWidthOfLabelsContainer)
         {
-            x = 0;
-            y += label->height() + spacing;
+            xCoordinateOfLabelInContainer = 0;
+            yCoordinateOfLabelInContainer += label->height() + spacingBetweenLabels;
         }
 
-        label->move(x, y);
+        label->move(xCoordinateOfLabelInContainer, yCoordinateOfLabelInContainer);
         label->show();
 
-        x += label->width();
-
-        if (i < numOfExpenses - 1)
-        {
-            QLabel *plusLabel = new QLabel("+", ui->groceriesLabelsContainer);
-            plusLabel->setStyleSheet("color: gray; font-size: 12px;");
-            plusLabel->adjustSize();
-            plusLabel->move(x + spacing - 2, y);
-            plusLabel->show();
-
-            x += plusLabel->width() + spacing;
-        }
+        xCoordinateOfLabelInContainer += label->width() + spacingBetweenLabels;
     }
-
-    ui->groceriesLabelsContainer->setMinimumHeight(y + 50);
+    labelsContainer->resize(maxWidthOfLabelsContainer, yCoordinateOfLabelInContainer + 20);
+    QPoint posRelativeToMainWindow = labelsContainer->mapTo(this, labelsContainer->rect().bottomLeft());
+    return posRelativeToMainWindow;
 }
 
 void MainWindow::loadXmlButtonClicked()
@@ -68,28 +103,29 @@ void MainWindow::loadXmlButtonClicked()
 bool MainWindow::eventFilter(QObject *watched, QEvent *event)
 {
     QLabel *label = qobject_cast<QLabel *>(watched);
-    if (label && priceLabels.contains(label))
+    if (label && expenseLabels.contains(label))
     {
         if (event->type() == QEvent::Enter)
         {
             label->setStyleSheet("color: #6750A4; font-family: Roboto; font-size: 12px; font-style: normal; font-weight: bold; border-radius: 5px; background: rgba(187, 134, 252, 0.20);");
-            QPoint pos = label->mapToGlobal(QPoint(0, 0));
-            pos = this->mapFromGlobal(pos);
-            ui->priceLabelDescription->move(pos.x(), pos.y() - ui->priceLabelDescription->height() - 5);
-            ui->priceLabelDescription->setText("01/04/2025 spozywka Lidl -12.99PLN<b>1400.99PLN</b>");
-            ui->priceLabelDescription->setVisible(true);
-            ui->priceLabelDescription->raise();
-            ui->priceLabelDescription->setStyleSheet("background: #DDDDDD; border-radius: 4px;");
+            QPoint posOfLabel = label->mapToGlobal(QPoint(0, 0));
+            posOfLabel = this->mapFromGlobal(posOfLabel);
+            ui->expenseDescriptionBanner->setFixedSize(465, 43);
+            ui->expenseDescriptionBanner->move(posOfLabel.x(), posOfLabel.y() - ui->expenseDescriptionBanner->height() - 5);
+            ui->expenseDescriptionBanner->setText("01/04/2025   spozywka   Lidl   -12.99PLN    <b>1400.99PLN</b>");
+            ui->expenseDescriptionBanner->setVisible(true);
+            ui->expenseDescriptionBanner->raise();
+            ui->expenseDescriptionBanner->setStyleSheet("background: #DDDDDD; border-radius: 4px;");
             auto *shadow = new QGraphicsDropShadowEffect(this);
             shadow->setBlurRadius(10);
             shadow->setOffset(0, 3);
             shadow->setColor(QColor(0, 0, 0, 36));
-            ui->priceLabelDescription->setGraphicsEffect(shadow);
+            ui->expenseDescriptionBanner->setGraphicsEffect(shadow);
         }
         else if (event->type() == QEvent::Leave)
         {
             label->setStyleSheet("color: #000000; font-family: Roboto; font-size: 12px; font-style: normal;");
-            ui->priceLabelDescription->setVisible(false);
+            ui->expenseDescriptionBanner->setVisible(false);
         }
     }
     return QMainWindow::eventFilter(watched, event);
--- mainwindow.h
+++ mainwindow.h
@@ -12,6 +12,7 @@
 #include <QFileDialog>
 #include <QGraphicsDropShadowEffect>
 #include <QLabel>
+#include <iostream>
 
 QT_BEGIN_NAMESPACE
 namespace Ui
@@ -30,13 +31,16 @@ public:
 
 private slots:
     void loadXmlButtonClicked();
+    QPoint drawExpensesLabels(QWidget *, QLabel *, int, const std::vector<std::string> &, QPoint);
 
 protected:
     bool eventFilter(QObject *watched, QEvent *event) override;
 
 private:
     Ui::MainWindow *ui;
-    QVector<QLabel *> priceLabels;
+    QVector<QLabel *> expenseLabels;
     QGridLayout *griLayout;
+    std::vector<QWidget *> allExpensesLabelsContainers;
+    std::vector<QLabel *> allExpensesLabelsHeadings;
 };
 #endif // MAINWINDOW_H
